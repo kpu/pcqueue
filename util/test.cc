28b6bbecf54d9c337a54e78f3f6f13244d950c4e
Kenneth Heafield <github@kheafield.com>
2021-04-21 21:49:42 +0100

    More rigorous tests

--- util/test.cc
+++ util/test.cc
@@ -1,5 +1,8 @@
 #include "pcqueue.h"
 
+#include <thread>
+#include <vector>
+
 namespace util {
 
 void Count() {
@@ -18,9 +21,52 @@ void Count() {
   }
 }
 
+void SingleInSingleOut() {
+  PCQueue<int> queue(15);
+  std::thread writer([&queue]() {
+      for (int i = 0; i < 100; ++i) {
+        queue.Produce(i);
+      }
+  });
+  for (int i = 0; i < 100; ++i) {
+    UTIL_ABORT_IF(i != queue.Consume(), Exception, "Mismatched PCQueue output for " << i);
+  }
+  writer.join();
+}
+
+void MultipleWriters() {
+  const unsigned kCount = 100;
+  PCQueue<unsigned> queue(13);
+  auto writer = [&queue, kCount]() {
+    for (unsigned i = 0; i < kCount; ++i) {
+      queue.Produce(i);
+    }
+  };
+  const unsigned kNumThreads = 3;
+  std::vector<std::thread> threads;
+  for (unsigned i = 0; i < kNumThreads; ++i) {
+    threads.emplace_back(writer);
+  }
+  unsigned seen[kCount] = {0};
+  for (unsigned i = 0; i < kCount * kNumThreads; ++i) {
+    unsigned got = queue.Consume();
+    UTIL_ABORT_IF(got >= kCount, Exception, "Index " << got << " too high.");
+    seen[got]++;
+  }
+  for (unsigned i = 0; i < kCount; ++i) {
+    UTIL_ABORT_IF(seen[i] != kNumThreads, Exception, "Expected to see values from exactly two threads but got " << seen[i] << " of " << i);
+  }
+  for (std::thread &t : threads) {
+    t.join();
+  }
+}
+
 } // namespace util
 
 int main() {
   util::Count();
-  std::cerr << "Success" << std::endl;
+  util::SingleInSingleOut();
+  util::MultipleWriters();
+  std::cout << "Success" << std::endl;
+  return 0;
 }
